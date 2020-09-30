d3670a6feeff1f8036630e344cd19c331f828e75
Javier Escacena Sánchez <fjescacena@paradigmadigital.com>
2020-09-30 22:35:50 +0200

    gatsby hello world

---
+++ reactjs/gatsby-hello-world/gatsby-config.js
@@ -0,0 +1,15 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/gatsby-config/
+ */
+
+module.exports = {
+    siteMetadata: {
+        title: `Gatsby`,
+        siteUrl: `https://www.gatsbyjs.com`,
+        description: `Blazing fast modern site generator for React`,
+    },
+
+    plugins: [],
+};
